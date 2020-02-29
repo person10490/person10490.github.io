d7a141694559242b6d7f8dac2114d19b2ce130fa
person10490 <61604738+person10490@users.noreply.github.com>
2020-02-29 17:07:55 +0000

    hello

---
+++ script.js
@@ -0,0 +1,280 @@
+var a = [{name:"CNG",GAS:0,DR:0,JKR:0,Geo:0,NS:0,FO:70000,RT:90000,GG:0,EP:100000,STR:0,PA:0,CLS:105000,OR:0,PS:70000,Clones:85000,S:0,R:60000,B:70000,IT:0,E:0,EN:12472,BH:100000},
+{name:"Roostar",GAS:0,DR:0,JKR:100000,Geo:850000,NS:85000,FO:85000,RT:0,GG:125000,EP:105000,STR:0,PA:130000,CLS:95000,OR:60000,PS:80000,Clones:150000,S:0,R:70000,B:0,IT:90000,E:85000,EN:18207,BH:110000},
+{name:"XSTRIKERX",GAS:0,DR:76000,JKR:115000,Geo:0,NS:70000,FO:105000,RT:85000,GG:75000,EP:105000,STR:0,PA:0,CLS:90000,OR:80000,PS:85000,Clones:0,S:0,R:70000,B:90000,IT:65000,E:70000,EN:0,BH:95000}
+/*{name:DarthBob,GAS:,DR:,JKR:,Geo:,NS:,FO:,RT:,GG:,EP:,STR:,PA:,CLS:,OR:,PS:,Clones:,S:,R:,B:,IT:,E:,EN:,BH:},
+{name:Smash,GAS:,DR:,JKR:,Geo:,NS:,FO:,RT:,GG:,EP:,STR:,PA:,CLS:,OR:,PS:,Clones:,S:,R:,B:,IT:,E:,EN:,BH:},
+{name:,GAS:,DR:,JKR:,Geo:,NS:,FO:,RT:,GG:,EP:,STR:,PA:,CLS:,OR:,PS:,Clones:,S:,R:,B:,IT:,E:,EN:,BH:},
+{name:,GAS:,DR:,JKR:,Geo:,NS:,FO:,RT:,GG:,EP:,STR:,PA:,CLS:,OR:,PS:,Clones:,S:,R:,B:,IT:,E:,EN:,BH:},
+{name:,GAS:,DR:,JKR:,Geo:,NS:,FO:,RT:,GG:,EP:,STR:,PA:,CLS:,OR:,PS:,Clones:,S:,R:,B:,IT:,E:,EN:,BH:},
+{name:,GAS:,DR:,JKR:,Geo:,NS:,FO:,RT:,GG:,EP:,STR:,PA:,CLS:,OR:,PS:,Clones:,S:,R:,B:,IT:,E:,EN:,BH:},
+{name:,GAS:,DR:,JKR:,Geo:,NS:,FO:,RT:,GG:,EP:,STR:,PA:,CLS:,OR:,PS:,Clones:,S:,R:,B:,IT:,E:,EN:,BH:},
+{name:,GAS:,DR:,JKR:,Geo:,NS:,FO:,RT:,GG:,EP:,STR:,PA:,CLS:,OR:,PS:,Clones:,S:,R:,B:,IT:,E:,EN:,BH:},
+{name:,GAS:,DR:,JKR:,Geo:,NS:,FO:,RT:,GG:,EP:,STR:,PA:,CLS:,OR:,PS:,Clones:,S:,R:,B:,IT:,E:,EN:,BH:},
+{name:,GAS:,DR:,JKR:,Geo:,NS:,FO:,RT:,GG:,EP:,STR:,PA:,CLS:,OR:,PS:,Clones:,S:,R:,B:,IT:,E:,EN:,BH:},
+{name:,GAS:,DR:,JKR:,Geo:,NS:,FO:,RT:,GG:,EP:,STR:,PA:,CLS:,OR:,PS:,Clones:,S:,R:,B:,IT:,E:,EN:,BH:},
+{name:,GAS:,DR:,JKR:,Geo:,NS:,FO:,RT:,GG:,EP:,STR:,PA:,CLS:,OR:,PS:,Clones:,S:,R:,B:,IT:,E:,EN:,BH:},
+{name:,GAS:,DR:,JKR:,Geo:,NS:,FO:,RT:,GG:,EP:,STR:,PA:,CLS:,OR:,PS:,Clones:,S:,R:,B:,IT:,E:,EN:,BH:},
+{name:,GAS:,DR:,JKR:,Geo:,NS:,FO:,RT:,GG:,EP:,STR:,PA:,CLS:,OR:,PS:,Clones:,S:,R:,B:,IT:,E:,EN:,BH:},
+{name:,GAS:,DR:,JKR:,Geo:,NS:,FO:,RT:,GG:,EP:,STR:,PA:,CLS:,OR:,PS:,Clones:,S:,R:,B:,IT:,E:,EN:,BH:},
+{name:,GAS:,DR:,JKR:,Geo:,NS:,FO:,RT:,GG:,EP:,STR:,PA:,CLS:,OR:,PS:,Clones:,S:,R:,B:,IT:,E:,EN:,BH:},
+{name:,GAS:,DR:,JKR:,Geo:,NS:,FO:,RT:,GG:,EP:,STR:,PA:,CLS:,OR:,PS:,Clones:,S:,R:,B:,IT:,E:,EN:,BH:},
+{name:,GAS:,DR:,JKR:,Geo:,NS:,FO:,RT:,GG:,EP:,STR:,PA:,CLS:,OR:,PS:,Clones:,S:,R:,B:,IT:,E:,EN:,BH:},
+{name:,GAS:,DR:,JKR:,Geo:,NS:,FO:,RT:,GG:,EP:,STR:,PA:,CLS:,OR:,PS:,Clones:,S:,R:,B:,IT:,E:,EN:,BH:},
+{name:,GAS:,DR:,JKR:,Geo:,NS:,FO:,RT:,GG:,EP:,STR:,PA:,CLS:,OR:,PS:,Clones:,S:,R:,B:,IT:,E:,EN:,BH:},
+{name:,GAS:,DR:,JKR:,Geo:,NS:,FO:,RT:,GG:,EP:,STR:,PA:,CLS:,OR:,PS:,Clones:,S:,R:,B:,IT:,E:,EN:,BH:},
+{name:,GAS:,DR:,JKR:,Geo:,NS:,FO:,RT:,GG:,EP:,STR:,PA:,CLS:,OR:,PS:,Clones:,S:,R:,B:,IT:,E:,EN:,BH:},
+{name:,GAS:,DR:,JKR:,Geo:,NS:,FO:,RT:,GG:,EP:,STR:,PA:,CLS:,OR:,PS:,Clones:,S:,R:,B:,IT:,E:,EN:,BH:},
+{name:,GAS:,DR:,JKR:,Geo:,NS:,FO:,RT:,GG:,EP:,STR:,PA:,CLS:,OR:,PS:,Clones:,S:,R:,B:,IT:,E:,EN:,BH:},
+{name:,GAS:,DR:,JKR:,Geo:,NS:,FO:,RT:,GG:,EP:,STR:,PA:,CLS:,OR:,PS:,Clones:,S:,R:,B:,IT:,E:,EN:,BH:},
+{name:,GAS:,DR:,JKR:,Geo:,NS:,FO:,RT:,GG:,EP:,STR:,PA:,CLS:,OR:,PS:,Clones:,S:,R:,B:,IT:,E:,EN:,BH:},
+{name:,GAS:,DR:,JKR:,Geo:,NS:,FO:,RT:,GG:,EP:,STR:,PA:,CLS:,OR:,PS:,Clones:,S:,R:,B:,IT:,E:,EN:,BH:},
+{name:,GAS:,DR:,JKR:,Geo:,NS:,FO:,RT:,GG:,EP:,STR:,PA:,CLS:,OR:,PS:,Clones:,S:,R:,B:,IT:,E:,EN:,BH:},
+{name:,GAS:,DR:,JKR:,Geo:,NS:,FO:,RT:,GG:,EP:,STR:,PA:,CLS:,OR:,PS:,Clones:,S:,R:,B:,IT:,E:,EN:,BH:},
+{name:,GAS:,DR:,JKR:,Geo:,NS:,FO:,RT:,GG:,EP:,STR:,PA:,CLS:,OR:,PS:,Clones:,S:,R:,B:,IT:,E:,EN:,BH:},
+{name:,GAS:,DR:,JKR:,Geo:,NS:,FO:,RT:,GG:,EP:,STR:,PA:,CLS:,OR:,PS:,Clones:,S:,R:,B:,IT:,E:,EN:,BH:},
+{name:,GAS:,DR:,JKR:,Geo:,NS:,FO:,RT:,GG:,EP:,STR:,PA:,CLS:,OR:,PS:,Clones:,S:,R:,B:,IT:,E:,EN:,BH:},
+{name:,GAS:,DR:,JKR:,Geo:,NS:,FO:,RT:,GG:,EP:,STR:,PA:,CLS:,OR:,PS:,Clones:,S:,R:,B:,IT:,E:,EN:,BH:},
+{name:,GAS:,DR:,JKR:,Geo:,NS:,FO:,RT:,GG:,EP:,STR:,PA:,CLS:,OR:,PS:,Clones:,S:,R:,B:,IT:,E:,EN:,BH:},
+{name:,GAS:,DR:,JKR:,Geo:,NS:,FO:,RT:,GG:,EP:,STR:,PA:,CLS:,OR:,PS:,Clones:,S:,R:,B:,IT:,E:,EN:,BH:},
+{name:,GAS:,DR:,JKR:,Geo:,NS:,FO:,RT:,GG:,EP:,STR:,PA:,CLS:,OR:,PS:,Clones:,S:,R:,B:,IT:,E:,EN:,BH:},
+{name:,GAS:,DR:,JKR:,Geo:,NS:,FO:,RT:,GG:,EP:,STR:,PA:,CLS:,OR:,PS:,Clones:,S:,R:,B:,IT:,E:,EN:,BH:},
+{name:,GAS:,DR:,JKR:,Geo:,NS:,FO:,RT:,GG:,EP:,STR:,PA:,CLS:,OR:,PS:,Clones:,S:,R:,B:,IT:,E:,EN:,BH:},
+{name:,GAS:,DR:,JKR:,Geo:,NS:,FO:,RT:,GG:,EP:,STR:,PA:,CLS:,OR:,PS:,Clones:,S:,R:,B:,IT:,E:,EN:,BH:},
+{name:,GAS:,DR:,JKR:,Geo:,NS:,FO:,RT:,GG:,EP:,STR:,PA:,CLS:,OR:,PS:,Clones:,S:,R:,B:,IT:,E:,EN:,BH:},
+{name:,GAS:,DR:,JKR:,Geo:,NS:,FO:,RT:,GG:,EP:,STR:,PA:,CLS:,OR:,PS:,Clones:,S:,R:,B:,IT:,E:,EN:,BH:},
+{name:,GAS:,DR:,JKR:,Geo:,NS:,FO:,RT:,GG:,EP:,STR:,PA:,CLS:,OR:,PS:,Clones:,S:,R:,B:,IT:,E:,EN:,BH:},
+{name:,GAS:,DR:,JKR:,Geo:,NS:,FO:,RT:,GG:,EP:,STR:,PA:,CLS:,OR:,PS:,Clones:,S:,R:,B:,IT:,E:,EN:,BH:},
+{name:,GAS:,DR:,JKR:,Geo:,NS:,FO:,RT:,GG:,EP:,STR:,PA:,CLS:,OR:,PS:,Clones:,S:,R:,B:,IT:,E:,EN:,BH:},
+{name:,GAS:,DR:,JKR:,Geo:,NS:,FO:,RT:,GG:,EP:,STR:,PA:,CLS:,OR:,PS:,Clones:,S:,R:,B:,IT:,E:,EN:,BH:},
+{name:,GAS:,DR:,JKR:,Geo:,NS:,FO:,RT:,GG:,EP:,STR:,PA:,CLS:,OR:,PS:,Clones:,S:,R:,B:,IT:,E:,EN:,BH:},
+{name:,GAS:,DR:,JKR:,Geo:,NS:,FO:,RT:,GG:,EP:,STR:,PA:,CLS:,OR:,PS:,Clones:,S:,R:,B:,IT:,E:,EN:,BH:}*/
+];
+
+
+
+
+function search(){
+
+var b=document.getElementById("teams").value;
+var x=document.getElementById("GP").value;
+var d=[];
+
+
+if (b=="GAS"){
+for(i=0;i<a.length;i++){
+if(a[i].GAS>=x)	{
+d.push(a[i].name);	
+d.push(a[i].GAS);
+}
+}
+}
+
+else if (b=="DR"){
+for(i=0;i<a.length;i++){
+if(a[i].DR>=x)	{
+d.push(a[i].name);	
+d.push(a[i].DR);
+}
+}
+}
+
+else if (b=="JKR"){
+for(i=0;i<a.length;i++){
+if(a[i].JKR>=x)	{
+d.push(a[i].name);	
+d.push(a[i].JKR);
+}
+}
+}
+
+else if (b=="Geo"){
+for(i=0;i<a.length;i++){
+if(a[i].Geo>=x)	{
+d.push(a[i].name);	
+d.push(a[i].Geo);
+}
+}
+}
+
+else if (b=="NS"){
+for(i=0;i<a.length;i++){
+if(a[i].NS>=x)	{
+d.push(a[i].name);	
+d.push(a[i].NS);
+}
+}
+}
+
+else if (b=="FO"){
+for(i=0;i<a.length;i++){
+if(a[i].FO>=x)	{
+d.push(a[i].name);	
+d.push(a[i].FO);
+}
+}
+}
+
+else if (b=="RT"){
+for(i=0;i<a.length;i++){
+if(a[i].RT>=x)	{
+d.push(a[i].name);	
+d.push(a[i].RT);
+}
+}
+}
+
+else if (b=="GG"){
+for(i=0;i<a.length;i++){
+if(a[i].GG>=x)	{
+d.push(a[i].name);	
+d.push(a[i].GG);
+}
+}
+}
+
+else if (b=="EP"){
+for(i=0;i<a.length;i++){
+if(a[i].EP>=x)	{
+d.push(a[i].name);	
+d.push(a[i].EP);
+}
+}
+}
+
+else if (b=="STR"){
+for(i=0;i<a.length;i++){
+if(a[i].STR>=x)	{
+d.push(a[i].name);	
+d.push(a[i].STR);
+}
+}
+}
+
+else if (b=="PA"){
+for(i=0;i<a.length;i++){
+if(a[i].PA>=x)	{
+d.push(a[i].name);	
+d.push(a[i].PA);
+}
+}
+}
+
+else if (b=="CLS"){
+for(i=0;i<a.length;i++){
+if(a[i].CLS>=x)	{
+d.push(a[i].name);	
+d.push(a[i].CLS);
+}
+}
+}
+
+else if (b=="OR"){
+for(i=0;i<a.length;i++){
+if(a[i].OR>=x)	{
+d.push(a[i].name);	
+d.push(a[i].OR);
+}
+}
+}
+
+else if (b=="PS"){
+for(i=0;i<a.length;i++){
+if(a[i].PS>=x)	{
+d.push(a[i].name);	
+d.push(a[i].PS);
+}
+}
+}
+
+else if (b=="Clones"){
+for(i=0;i<a.length;i++){
+if(a[i].Clones>=x)	{
+d.push(a[i].name);	
+d.push(a[i].Clones);
+}
+}
+}
+
+else if (b=="S"){
+for(i=0;i<a.length;i++){
+if(a[i].S>=x)	{
+d.push(a[i].name);	
+d.push(a[i].S);
+}
+}
+}
+
+else if (b=="R"){
+for(i=0;i<a.length;i++){
+if(a[i].R>=x)	{
+d.push(a[i].name);	
+d.push(a[i].R);
+}
+}
+}
+
+else if (b=="B"){
+for(i=0;i<a.length;i++){
+if(a[i].B>=x)	{
+d.push(a[i].name);	
+d.push(a[i].B);
+}
+}
+}
+
+else if (b=="IT"){
+for(i=0;i<a.length;i++){
+if(a[i].IT>=x)	{
+d.push(a[i].name);	
+d.push(a[i].IT);
+}
+}
+}
+
+else if (b=="EN"){
+for(i=0;i<a.length;i++){
+if(a[i].EN>=x)	{
+d.push(a[i].name);	
+d.push(a[i].EN);
+}
+}
+}
+
+else if (b=="E"){
+for(i=0;i<a.length;i++){
+if(a[i].E>=x)	{
+d.push(a[i].name);	
+d.push(a[i].E);
+}
+}
+}
+else if (b=="BH"){
+for(i=0;i<a.length;i++){
+if(a[i].BH>=x)	{
+d.push(a[i].name);	
+d.push(a[i].BH);
+}
+}
+}
+
+
+var e="";
+var counter=0;
+for (i=0;i<d.length;i++){
+	
+
+	e=e+d[i]
+if (counter==0){
+counter+=1;
+e=e+" ";
+}
+else{
+counter-=1;
+e=e+"</br>";
+
+}
+}
+document.getElementById("demo").innerHTML=e;
+
+}
+
